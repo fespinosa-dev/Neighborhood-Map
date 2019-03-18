a073300600ffdb72acde8dd35c3bb83fc4f49e9c
Fernando Espinosa <fjevictoriano@gmail.com>
2019-03-18 12:04:33 -0400

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/normalize.css'
-import './css/index.css';
-import App from './App';
-import  register  from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-register();
